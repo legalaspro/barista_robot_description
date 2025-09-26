fcf787123632beb948ea69480af29a7b0d21a159
Dmitri Manajev <dmitri.manajev@protonmail.com>
2025-09-26 08:18:58 +0000

    Added explicitly frame to laserscan, added wait for gazebo load before rviz launch.

--- launch/barista_urdf.launch.py
+++ launch/barista_urdf.launch.py
@@ -1,9 +1,10 @@
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
 from launch.substitutions import Command, LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch.event_handlers import OnProcessExit
 from ament_index_python.packages import get_package_share_directory
 from ament_index_python.packages import get_package_prefix
 
@@ -35,7 +36,7 @@ def generate_launch_description():
 
     # Declare a new launch argument for the world file
     # world_file = "empty.sdf"
-    # world_file_arg = DeclareLaunchArgument(
+    # declare_world_arg = DeclareLaunchArgument(
     #     'world',
     #     default_value=["-r ", world_file],
     #     description='Path to the Gazebo world file'
@@ -62,7 +63,6 @@ def generate_launch_description():
     robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", urdf_file)
 
     # Robot State Publisher
-
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -89,6 +89,14 @@ def generate_launch_description():
                         arguments=['-entity', 'barista_robot', '-x', '0.0', '-y', '0.0', '-z', '0.2',
                                    '-topic', 'robot_description'],
                         output='screen')
+    
+    # Event handler to launch RViz after spawn_robot exits
+    delayed_rviz = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_robot,
+            on_exit=[rviz_node],
+        )
+    )
 
     # create and return launch description object
     return LaunchDescription(
@@ -96,6 +104,6 @@ def generate_launch_description():
             gazebo,
             robot_state_publisher_node,
             spawn_robot,
-            rviz_node
+            delayed_rviz
         ]
     )
