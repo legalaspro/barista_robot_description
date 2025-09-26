33010fac7d90a025958514af191107d7962ca499
Dmitri Manajev <dmitri.manajev@protonmail.com>
2025-09-26 12:23:24 +0000

    Added Gazebo+Rviz launch for xacro baristo robot.

---
+++ launch/barista_xacro.launch.py
@@ -0,0 +1,108 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.substitutions import Command, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.event_handlers import OnProcessExit
+from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import get_package_prefix
+
+
+# this is the function launch  system will look for
+def generate_launch_description():
+
+    package_description = "barista_robot_description"
+    install_dir = get_package_prefix(package_description)
+
+    # This is to find the models
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        os.environ['GAZEBO_MODEL_PATH'] = os.environ['GAZEBO_MODEL_PATH'] + \
+            ':' + install_dir + '/share'
+    else:
+        os.environ['GAZEBO_MODEL_PATH'] = install_dir + \
+            "/share"
+
+    if 'GAZEBO_PLUGIN_PATH' in os.environ:
+        os.environ['GAZEBO_PLUGIN_PATH'] = os.environ['GAZEBO_PLUGIN_PATH'] + \
+            ':' + install_dir + '/lib'
+    else:
+        os.environ['GAZEBO_PLUGIN_PATH'] = install_dir + '/lib'
+
+    print("GAZEBO MODELS PATH=="+str(os.environ["GAZEBO_MODEL_PATH"]))
+    print("GAZEBO PLUGINS PATH=="+str(os.environ["GAZEBO_PLUGIN_PATH"]))
+
+    # use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+    # Declare a new launch argument for the world file
+    # world_file = "empty.sdf"
+    # declare_world_arg = DeclareLaunchArgument(
+    #     'world',
+    #     default_value=["-r ", world_file],
+    #     description='Path to the Gazebo world file'
+    # )
+
+    # Define the launch arguments for the Gazebo launch file
+    gazebo_launch_args = {
+        'verbose': 'false',
+        'pause': 'false',
+        # 'world': LaunchConfiguration('world')
+    }
+
+    # Include the Gazebo launch file with the modified launch arguments
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+        launch_arguments=gazebo_launch_args.items(),
+    )
+
+    # Define the robot model files to be used
+    print("Fetching URDF ==>")
+    urdf_file = 'barista_robot_model.urdf.xacro'
+    robot_desc_path = os.path.join(get_package_share_directory(package_description), "xacro", urdf_file)
+
+    # Robot State Publisher
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher_node',
+        emulate_tty=True,
+        parameters=[{'use_sim_time': True, 'robot_description': Command(['xacro ', robot_desc_path])}],
+        output="screen"
+    )
+
+    # RVIZ Configuration
+    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'xacro_vis.rviz')
+
+    rviz_node = Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            name='rviz_node',
+            parameters=[{'use_sim_time': True}],
+            arguments=['-d', rviz_config_dir]
+    )
+    
+    # # Spawn robot 
+    spawn_robot = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-entity', 'barista_robot', '-x', '0.0', '-y', '0.0', '-z', '0.2',
+                                   '-topic', 'robot_description'],
+                        output='screen')
+    
+    # Event handler to launch RViz after spawn_robot exits
+    delayed_rviz = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_robot,
+            on_exit=[rviz_node],
+        )
+    )
+
+    # create and return launch description object
+    return LaunchDescription(
+        [            
+            gazebo,
+            robot_state_publisher_node,
+            spawn_robot,
+            delayed_rviz
+        ]
+    )
